219e94be0b210c79021c4f76a128da9ae51e6fff
TidusDotExe <tidusdotexe@gmail.com>
2021-01-17 00:51:23 +0100

    modified link import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/Head'
 import Layout from '../components/Layout'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 export default function Home({pokemons}) {
   return (
